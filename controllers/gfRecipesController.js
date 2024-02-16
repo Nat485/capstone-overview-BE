82b9da1437a419c22a20f9b1fffb6c6118a17823
Nat <nat485@gmail.com>
2024-02-16 13:59:56 -0500

    added controller code

--- controllers/gfRecipesController.js
+++ controllers/gfRecipesController.js
@@ -8,5 +8,30 @@ gfRecipes.get("/", (req, res) => {
     res.status(200).json({message:"GF recipes homepage"})
 
 })
+//adding the id for the items that will exist in the website
+
+gfRecipes.get("/:recipeID",(req, res) => {
+    const recipeID = req.params.recipeID
+    
+    console.log(Number(recipeID))
+    if(Number(recipeID)){
+        res.status(200).json({message: recipeID})
+    }
+    else{
+        res.status(404).json({
+            error : "id must be numeric value"
+        })
+    }
+    
+    
+    //console.log(recipeID, "param")
+
+    res.status(200).json({message: recipeID})
+})
+
+
+//the req is an object; it is dissecting all of the different urls
+
+
 
 module.exports = gfRecipes
