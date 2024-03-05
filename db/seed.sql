996acacda70eedfc337e647a7740a2b5a5b88f77
Nat <nat485@gmail.com>
2024-03-05 12:05:32 -0500

    fixed issues

--- db/seed.sql
+++ db/seed.sql
@@ -2,4 +2,4 @@
 INSERT INTO recipes (recipe_name, creation_date, vegan, recipe_type) 
 VALUES ('Crispy Oyster Mushrooms', 2020, TRUE, 'MainDish'),
 ('Eggplant Rollatini', 2019, FALSE, 'Apps'),
-('Plantain Empandas, 2018, FALSE, 'Apps');
+('Plantain Empandas', 2018, FALSE, 'Apps');
